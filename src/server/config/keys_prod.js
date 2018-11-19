81fbd267f4b93c5d8b4746d30e40c06552d51f72
Misbauddin Chowdhury Shahan <shahan953@hotmail.com>
2018-11-19 23:41:06 +0600

    NNN

--- src/server/config/keys_prod.js
+++ src/server/config/keys_prod.js
@@ -1,4 +1,9 @@
+// module.exports = {
+//   mongoURI: process.env.MONGO_URI,
+//   secretOrKey: process.env.SECRET_OR_KEY
+// };
 module.exports = {
-  mongoURI: process.env.MONGO_URI,
-  secretOrKey: process.env.SECRET_OR_KEY
+  mongoUri: 'mongodb://arup-rd:01738arup@ds139992.mlab.com:39992/devcommunity',
+  secretOrKey: 'secret'
 };
+
